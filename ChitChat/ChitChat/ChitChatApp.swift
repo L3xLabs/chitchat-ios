dfb5e044140ff870adbf06ed3df58eb5dd47191e
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-10-19 20:05:13 +0530

    matrix-ios-sdk

---
+++ ChitChat/ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Puchi on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
